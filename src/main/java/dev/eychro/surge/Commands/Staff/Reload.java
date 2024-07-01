a750aa5505bf32c4c0253de8be4a21f3d6691510
eychro <HaroldRodas09@gmail.com>
2024-06-30 22:27:43 -0600

    Added command to reload configuration

---
+++ src/main/java/dev/eychro/surge/Commands/Staff/Reload.java
@@ -0,0 +1,18 @@
+package dev.eychro.surge.Commands.Staff;
+
+import dev.eychro.surge.Handlers.CustomConfiguration;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class Reload implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
+        if (sender.hasPermission("Surge.Reload")) {
+            CustomConfiguration.reload();
+        } else {
+            sender.sendMessage(CustomConfiguration.get().getString("Prefix") + CustomConfiguration.get().getString("NoPermission"));
+        }
+        return true;
+    }
+}
